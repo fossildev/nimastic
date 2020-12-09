c93cb0b5b0a2c2c45df2cf15e7d08c9f06beb88b
chandra agung rizky <needkopi@gmail.com>
2020-12-09 21:51:37 +0700

    add example for cat anomaly detector

---
+++ example/catanomalydetector.nim
@@ -0,0 +1,14 @@
+import nimastic, httpclient
+
+var client = elasticBasicClient()
+
+let req = elasticsearch.CatAnomalyDitectors(
+        Format: "json",
+        Bytes: "gb",
+        H: @["buckets.time.exp_avg_hour","buckets.time.max","data.input_fields"],
+        Time: "s" # s to second
+    )
+
+let res = req.Do(client)
+
+echo res.body
